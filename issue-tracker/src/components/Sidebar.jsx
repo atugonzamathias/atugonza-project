d7b5f14929278bde485943df91f42641c7c505bd
Mathias Atugonza <atumathias20@gmail.com>
2025-05-16 02:30:59 +0300

    added

--- issue-tracker/src/components/Sidebar.jsx
+++ issue-tracker/src/components/Sidebar.jsx
@@ -48,32 +48,9 @@ const Sidebar = () => {
                 <span class="flex-1 ms-3 whitespace-nowrap">Create Issue</span>
               </Link>
             </li>
-
             <li>
-
             </li>
             <LogoutButton />
-
-            <li>
-              <a
-                href="#"
-                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#e3e3e3"
-                >
-                  <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
-                </svg>
-                <span class="flex-1 ms-3 whitespace-nowrap">Notifications</span>
-                <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  3
-                </span>
-              </a>
-            </li>
           </ul>
           <NotificationBell />
         </div>
